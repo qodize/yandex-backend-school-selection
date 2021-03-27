cdacba77041fbe34f87f0a5cb38a7e1dd757c0e9
qodize <qodize@gmail.com>
2021-03-28 01:51:46 +0500

    back_to_start

--- yandex_backend/settings.py
+++ yandex_backend/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'delivery.apps.DeliveryConfig',
-    'django_ssh_deployer'
+    'delivery.apps.DeliveryConfig'
 ]
 
 MIDDLEWARE = [
@@ -76,23 +75,13 @@ WSGI_APPLICATION = 'yandex_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'd1m7cjg5kearig',
-#         'USER': 'jeepgwimlhbsjd',
-#         'PASSWORD': 'a8474aad08ea8d8299f5633178db07f92182d3b111054364170e1a169f913f0b',
-#         'HOST': 'ec2-54-220-170-192.eu-west-1.compute.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'yandex_django',
-        'USER': 'yandex_django',
-        'PASSWORD': '121453qwe',
-        'HOST': '127.0.0.1',
+        'NAME': 'd1m7cjg5kearig',
+        'USER': 'jeepgwimlhbsjd',
+        'PASSWORD': 'a8474aad08ea8d8299f5633178db07f92182d3b111054364170e1a169f913f0b',
+        'HOST': 'ec2-54-220-170-192.eu-west-1.compute.amazonaws.com',
         'PORT': '5432',
     }
 }
