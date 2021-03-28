a43f291435fed31f130d11a971c060f65defd036
qodize <qodize@gmail.com>
2021-03-28 19:50:42 +0000

    server configuration

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from delivery.models import Courier, Order
 
 # Register your models here.
+admin.site.register(Courier)
+admin.site.register(Order)
--- yandex_backend/settings.py
+++ yandex_backend/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for yandex_backend project.
 
@@ -11,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'jjc_=1rwvd9*2^+z4acybrdye$s9zr(&7y^ksp$lbo#1+&+3gx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['178.154.194.31']
 
 
 # Application definition
@@ -121,5 +123,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
